af21cf342d12d52f92c4664c04317db4accfc2cf
Rakeshraki46 <141985198+Rakeshraki46@users.noreply.github.com>
2023-11-03 09:32:58 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,14 @@
 provider "aws" {
- region = "us-east-2"
- access_key = "AKIAXNJHMXFKIPCGELPQ"
- secret_key = "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"
+ region = "us-west-2"
+ access_key = "AKIAXNJHMXFKPPTV6BOB"
+ secret_key = "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"
 }
-resource "aws_instance" "mudhiraj" {
- ami = "ami-0e83be366243f524a"
+
+resource "aws_instance" "terra" {
+ ami = "ami-0efcece6bed30fd98"
  instance_type = "t2.micro"
  count = 2
 tags = {
-       Name = "Raki"
+      Name = "raki"
 }
 }
-
